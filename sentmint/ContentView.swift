1322a9ca574a47f193c836d98da1e22635346ff6
yara <yara.m.qlhatani@gmail.com>
2023-01-05 08:02:30 +0300

    Initial Commit

---
+++ sentmint/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  sentmint
+//
+//  Created by yara mohammed alqahtani on 12/06/1444 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ sentmint/sentmintApp.swift
@@ -0,0 +1,17 @@
+//
+//  sentmintApp.swift
+//  sentmint
+//
+//  Created by yara mohammed alqahtani on 12/06/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct sentmintApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
